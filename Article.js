a8288ad42973e6469b5b68d3bd3acbc7484facea
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-04-05 13:47:56 +0200

    article tab and text qty area with id

--- Article.js
+++ Article.js
@@ -1,3 +1,5 @@
+
+
 class Article {
 /*     id
     name
@@ -12,12 +14,13 @@ class Article {
         this.quantity = quantity;
     }
 }
-
-const Article = new Article(1,'MacBook', 'Apple',	2000, 0);
-const Article = new Article(2,'S10', 'Samsung',	700, 0);
-const Article = new Article(3,'Iphone150', 'Apple',	700, 0);
-const Article = new Article(4,'Laptop'      , 	'PH',	1199 , 0);
-const Article = new Article(5,'S8','Samsung',200,0);
-const Article = new Article(6,'S9','Samsung',200,0);
-const Article = new Article(7,'Iphone19','Apple',300,0);
-const Article = new Article(7,'ipad','Apple',400,0);
+let articles = [
+new Article(1,'MacBook', 'Apple',	2000, 0),
+new Article(2,'S10', 'Samsung',	700, 0),
+new Article(3,'Iphone150', 'Apple',	700, 0),
+new Article(4,'Laptop'      , 	'PH',	1199 , 0),
+new Article(5,'S8','Samsung',200,0),
+new Article(6,'S9','Samsung',200,0),
+new Article(7,'Iphone19','Apple',300,0),
+new Article(8,'ipad','Apple',400,0),
+]
--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-import { Article } from './Article';
 
-const menuSection = document.querySelector("#menuSection");
-const articlesSection = document.querySelector("#articlesSection");
-const cartSection = document.querySelector("#cartSection");
-const custInfoSectionSection = document.querySelector("#custInfoSectionSection");
-const cmdFinalizeSection = document.querySelector("#cmdFinalizeSection");
 
-const states = {nav, selecting, purchasing, finalizing, dev}
+class Article {
+    /*     id
+        name
+        brand
+        price
+        quantity */
+        constructor(id, name, brand, price, quantity){
+            this.id = id;
+            this.name = name;
+            this.brand = brand;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+    let articles = [
+    new Article(1,'MacBook', 'Apple',	2000, 0),
+    new Article(2,'S10', 'Samsung',	700, 0),
+    new Article(3,'Iphone150', 'Apple',	700, 0),
+    new Article(4,'Laptop'      , 	'PH',	1199 , 0),
+    new Article(5,'S8','Samsung',200,0),
+    new Article(6,'S9','Samsung',200,0),
+    new Article(7,'Iphone19','Apple',300,0),
+    new Article(8,'ipad','Apple',400,0),
+    ]
+/**************************************************************** */    
 
-console.log(`menuSection${menuSection}`)
+
+
+const menuSection = document.querySelector('#menuSection');
+const articlesSection = document.querySelector('#articlesSection');
+const cartSection = document.querySelector('#cartSection');
+const custInfoSectionSection = document.querySelector('#custInfoSectionSection');
+const cmdFinalizeSection = document.querySelector('#cmdFinalizeSection');
+
+/***********************************************************************************/
+/*****************************affichage/**************************************/
+/***********************************************************************************/
+/* const states = ["nav", "selecting", "purchasing", "finalizing", "dev"];
+states=;
+
  switch(states) {
     case nav:
         menuSection.style.visibility='visible'
@@ -49,3 +80,58 @@ console.log(`menuSection${menuSection}`)
         console.log(`Sorry, we are out of ${states}.`);
 
 } 
+
+ */
+articles.forEach((item, index) => {
+    console.log(item) //value
+    console.log(index) //index
+  }) 
+  
+
+addTableContent(articles);
+/***********************************************************************************/
+/*****************************gestion affichage tab**************************************/
+/***********************************************************************************/
+function addTableContent() {
+
+    var tblBody = document.querySelector("#articles__body__articles__tab");
+    console.log("tblBody" + tblBody);
+    // creating all cells
+    for (var i = 0; i < articles.length; i++) {
+      // creates a table row
+      var row = document.createElement("tr");
+  
+      for (var j = 0; j < 5; j++) {
+        // Create a <td> element and a text node, make the text
+        // node the contents of the <td>, and put the <td> at
+        // the end of the table row
+        var cell = document.createElement("td");
+        let k;
+        switch(j){
+            case 0: var cellText = document.createTextNode(articles[i].id);
+                break;
+                case 1:var cellText = document.createTextNode(articles[i].name);
+                break;
+                case 2:var cellText = document.createTextNode(articles[i].brand);
+                break;
+                case 3:var cellText = document.createTextNode(articles[i].price);
+                break;
+                case 4:var cellText = document.createElement("input");
+                cellText.setAttribute("type","text");
+                cellText.setAttribute("class","widht50");
+                const inputId = `quantityTextId${articles[i].id}`;
+                cellText.setAttribute("id",inputId);
+                break;
+                default:
+                    console.log("error j articles")
+        }
+        /* var text = `articles[i].${k}`; */
+        
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+    }
+  }
+  
